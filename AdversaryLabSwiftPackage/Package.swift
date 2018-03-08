015560cdcb2b71922ed7d6b31313e81a16d47387
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2018-03-07 21:29:50 -0600

    Require the modern sockets version of Redshot

--- AdversaryLabSwiftPackage/Package.swift
+++ AdversaryLabSwiftPackage/Package.swift
@@ -1,17 +1,13 @@
 // swift-tools-version:4.0
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AdversaryLabSwiftPackage",
     dependencies: [
-        // Dependencies declare other packages that this package depends on.
-         .package(url: "https://github.com/OperatorFoundation/Auburn.git", from: "0.1.5"),
+         .package(url: "https://github.com/OperatorFoundation/Auburn.git", from: "0.1.6"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "AdversaryLabSwiftPackage",
             dependencies: ["Auburn"]),
