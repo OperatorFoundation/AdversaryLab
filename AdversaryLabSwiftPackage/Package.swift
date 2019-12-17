4d3e0757423f4874cf4c54a140bd00d77a11ae0c
Consuelita <mafalda@Mafaldas-MacBook-Pro.local>
2019-12-17 10:15:49 -0600

    Added Rethink Dependency

--- AdversaryLabSwiftPackage/Package.swift
+++ AdversaryLabSwiftPackage/Package.swift
@@ -5,6 +5,7 @@ import PackageDescription
 let package = Package(
     name: "AdversaryLabSwiftPackage",
     dependencies: [
+        .package(url: "https://github.com/OperatorFoundation/rethink-swift", from: "1.1.0"),
          .package(url: "https://github.com/OperatorFoundation/Auburn.git", from: "0.6.0"),
          .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "1.0.5"),
          .package(url: "https://github.com/weichsel/ZIPFoundation/", .upToNextMajor(from: "0.9.0")),
@@ -13,7 +14,7 @@ let package = Package(
     targets: [
         .target(
             name: "AdversaryLabSwiftPackage",
-            dependencies: ["Auburn", "Datable", "ZIPFoundation", "Charts"]),
+            dependencies: ["Rethink", "Auburn", "Datable", "ZIPFoundation", "Charts"]),
         .testTarget(
             name: "AdversaryLabSwiftPackageTests",
             dependencies: ["AdversaryLabSwiftPackage"]),
